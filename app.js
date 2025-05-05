c78ab201cb5454fa67dc66b702241ea7b638279c
Muhammad Faiq <muhammadfaiqwaseem3@gmail.com>
2025-05-05 15:28:34 +0500

    my palaindrome

---
+++ app.js
@@ -0,0 +1,58 @@
+function palaindrome() {
+    var userInput = document.getElementById("usertext").value
+    var contcatStr = "";
+    for (var i = userInput.length - 1; i >= 0; i--) {
+        console.log(userInput[i])
+        contcatStr += userInput[i]
+    }
+    console.log(contcatStr)
+    if (userInput.toLowerCase() == contcatStr.toLowerCase()) {
+        document.getElementById("paltext").innerText = "PALAINDROME";
+    } else {
+        document.getElementById("paltext").innerText = "NOT PALAINDROME";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function palaindrome(){
+//     var userInput = document.getElementById("usertext").value;
+// var str ="";
+//     for(var i = userInput.length -1; i >= 0; i--){
+//         console.log(userInput[i])
+//         str += userInput[i]
+//     }
+//     if(userInput.toLowerCase() == str.toLowerCase()){
+//         document.getElementById("paltext").innerText="palaindrome"
+//     }else{
+//         document.getElementById("paltext").innerText= "Not palaindrome"
+
+//     }
+
+// }
